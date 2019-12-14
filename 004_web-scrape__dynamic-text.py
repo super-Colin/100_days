7c98d067b7fec7afda635dbb9fe8b81f9d9b758b
superColin <supercolin.dev@gmail.com>
2019-12-14 18:29:50 -0500

    day 4

---
+++ 004_web-scrape__dynamic-text.py
@@ -0,0 +1,36 @@
+# 12/13/2019
+# Learning Python
+
+import sys
+from PyQt4.QtGui import QApplication
+from PyQt4.QtCore import QUrl
+from PyQt4.QtWebKit import QWebPage
+
+import bs4 as bs
+import urllib.request
+# source  =urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/')
+# soup = bs.BeautifulSoup(source, 'lxml')
+
+# js_test = soup.find('p', class_='jstest')
+
+# print(js_test.text)
+
+
+class Client(QWebPage):
+
+    def __init__(self, url):
+        self.app = QApplication(sys.argv)
+        QWebPage.__init__(self)
+        self.loadFinished.connect(self.on_page_load)
+        self.mainFrame().load(QUrl(url))
+        self.app.exec_()
+
+    def on_page_load(self):
+        self.app.quit()
+
+url = 'https://pythonprogramming.net/parsememcparseface/'
+client_response = Client(url)
+source = client_response.mainFrame().toHtml()
+soup = bs.BeautifulSoup(source, 'lxml')
+js_test = soup.find('p', class_='jstest')
+print(js_test.text)
