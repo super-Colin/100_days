d8456d84629cecd4d81420f770986ec0112d5b0c
superColin <supercolin.dev@gmail.com>
2019-12-21 16:08:05 -0500

    day 11, git and shell script

--- 011__git-clone.sh
+++ 011__git-clone.sh
@@ -1,18 +1,18 @@
 # 12/21/19
 # Learning Bash
 
-# MOUNT THE FOLDER
+# MOUNT THE THEMES FOLDER
 cd /var/www/html/wp-content/themes
 
 # DELETE OLD VERSION
-FILE=/var/www/html/wp-content/themes/supercolin-theme
+FILE=/var/www/html/wp-content/themes/git_repo-theme
 if [ -e "$FILE" ]; then
     rm -rf $FILE
 fi
 
 
 # CLONE REPO
-sudo git clone https://github.com/super-Colin/supercolin-theme.git
+sudo git clone https://github.com/super-Colin/git_repo-theme.git
 
 
 # WILL ASK FOR GIT USER AND PASS HERE
--- 011__rm-old-file.sh
+++ 011__rm-old-file.sh
@@ -8,26 +8,26 @@ cd /var/www/html/wp-content/themes
 
 
 # # RENAME OLD VERSION
-# FILE=/var/www/html/wp-content/themes/supercolin-theme
+# FILE=/var/www/html/wp-content/themes/git_repo-theme
 # if [ -e "$FILE" ]; then
-#     mv $FILE /var/www/html/wp-content/themes/supercolin__old
+#     mv $FILE /var/www/html/wp-content/themes/git_repo__old
 # fi
 
 # ls -l
 
 
 # # DELETE OLD VERSION
-# # FILE = /var/www/html/wp-content/themes/supercolin__old
+# # FILE = /var/www/html/wp-content/themes/git_repo__old
 # if [ -e "$FILE" ]; then
 #     rm -rf $FILE
 # else
 #     echo "the old FILE var wasn't findable"
-#     FILE_NEW = ./supercolin__old
+#     FILE_NEW = ./git_repo__old
 #     rm -rf $FILE_NEW
 # fi
 
 # RENAME OLD VERSION
-FILE=/var/www/html/wp-content/themes/supercolin-theme
+FILE=/var/www/html/wp-content/themes/git_repo-theme
 if [ -e "$FILE" ]; then
     rm -rf $FILE
 fi
