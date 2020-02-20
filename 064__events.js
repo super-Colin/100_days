bdc8bc4253f8d081ed54fb79f36783cc69755bc9
superColin <supercolin.dev@gmail.com>
2020-02-19 20:40:49 -0500

    day 64

---
+++ 064__events.js
@@ -0,0 +1,22 @@
+// 2/19/2020
+// another method of the day!
+
+// https://websitesetup.org/javascript-cheat-sheet/
+
+function testOnContextMenu() {
+    document.getElementById('info-text').innerHTML = '';
+    document.getElementById('output-text').innerHTML = '';
+    document.getElementById('output-text2').innerHTML = '';
+    document.getElementById('output-text3').innerHTML = '';
+    alert('You right clicked for the context menu!!')
+}
+
+
+
+function testOnDblClick() {
+    document.getElementById('info-text').innerHTML = '';
+    document.getElementById('output-text').innerHTML = '';
+    document.getElementById('output-text2').innerHTML = '';
+    document.getElementById('output-text3').innerHTML = '';
+    alert('You double clicked!!')
+}
