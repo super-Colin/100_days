f3e459ca3891656c994c93af2c37ad1f51effa4c
superColin <supercolin.dev@gmail.com>
2020-02-21 14:38:06 -0500

    day 66

---
+++ 066__keyboard-events.js
@@ -0,0 +1,12 @@
+// 2/21/2020
+// another method of the day!
+
+// https://websitesetup.org/javascript-cheat-sheet/
+
+
+function testOnKeyDown() {
+    document.getElementById('keyboard-input').style.cssText = 'background-color: white; color: black'
+}
+function testOnKeyUp() {
+    document.getElementById('keyboard-input').style.cssText = 'background-color: purple; color: white'
+}
