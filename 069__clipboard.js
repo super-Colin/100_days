1f1bf351341da3b5a5889c6bd7ba9d4d121edbc9
superColin <supercolin.dev@gmail.com>
2020-02-24 11:46:39 -0500

    day 69!

---
+++ 069__clipboard.js
@@ -0,0 +1,17 @@
+// 2/24/2020
+// another method of the day!
+
+// https://websitesetup.org/javascript-cheat-sheet/
+
+
+function testOnCopy(){
+    alert('You copied text!');
+}
+
+function testOnCut() {
+    alert('You cut text!');
+}
+
+function testOnPaste(){
+    alert('You pasted text!');
+}
