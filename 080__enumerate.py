8ecec2a59cb2a408411d21e14aa0adba5386b262
superColin <supercolin.dev@gmail.com>
2020-03-06 09:19:12 -0500

    day 80

---
+++ 080__enumerate.py
@@ -0,0 +1,17 @@
+# 3/06/2020
+
+# https://docs.python.org/3/library/functions.html
+
+numbers = [0,1,2,2,3,3,4,4,55,6,7,8,9]
+strings = ['string 1', 'string 2 ', 'another', 'string']
+
+output = list(enumerate(strings))
+
+# print(enumerate(numbers))
+# list(enumerate(numbers))
+# print(enumerate(strings))
+# list(enumerate(strings))
+
+print(output)
+# list(str(output))
+print(list(enumerate(numbers)))
