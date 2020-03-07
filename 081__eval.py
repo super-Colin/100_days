bb62a33719d751a52217cc25e570173bd347ec77
superColin <supercolin.dev@gmail.com>
2020-03-07 10:07:56 -0500

    day 81

---
+++ 081__eval.py
@@ -0,0 +1,9 @@
+# 3/07/2020
+
+# https://docs.python.org/3/library/functions.html
+
+numbers = [0,1,2,2,3,3,4,4,55,6,7,8,9]
+
+eval('print(list(enumerate(numbers)))')
+
+# Pretty much the same as the Javascript eval function
