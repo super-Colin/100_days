9c4915f82ce3130b9914675e2e22da50ef8b6265
superColin <supercolin.dev@gmail.com>
2020-03-11 14:14:41 -0400

    day 86

---
+++ 086__input.py
@@ -0,0 +1,15 @@
+# 3/11/2020
+
+# https://docs.python.org/3/library/functions.html
+# https://docs.python.org/3/library/functions.html
+
+
+# words = input('something ')
+print('enter anything')
+words = raw_input()
+# words
+print(' dont throw an error over : ' + words)
+
+
+# x = raw_input('Enter your name:')
+# print('Hello, ' + x)
