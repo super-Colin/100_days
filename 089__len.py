dcd8beb758905534911636247cb50c964f5fe9a4
superColin <supercolin.dev@gmail.com>
2020-03-14 22:10:35 -0400

    day 89

---
+++ 089__len.py
@@ -0,0 +1,12 @@
+# 3/14/2020
+
+# https://docs.python.org/3/library/functions.html
+
+# list of vowels
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+
+print(len(vowels)) # returns 5 but array still has a zero index 
+# so to access the last item will need to -1 to the len() result
+print(vowels[len(vowels) - 1])
+
