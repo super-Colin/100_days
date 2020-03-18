b6a1e6ead9be6c8699d01eeda31f3714f47a330e
superColin <supercolin.dev@gmail.com>
2020-03-18 15:55:09 -0400

    day 93!

---
+++ 093__ord.py
@@ -0,0 +1,16 @@
+# 3/18/2020
+
+# https://docs.python.org/3/library/functions.html
+  
+
+
+
+print('Unicode for a is: ' + str(ord('a')) )
+print('Unicode for 1 is: ' + str(ord('1')) )
+print('Unicode for ! is: ' + str(ord('!')) )
+print('Unicode for @ is: ' + str(ord('@')) )
+print('Unicode for # is: ' + str(ord('#')) )
+print('Unicode for $ is: ' + str(ord('$')) )
+
+
+# returns as an int so must be made to string
