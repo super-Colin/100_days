885e08e9d1c4f101a7b330cbb440ac6adbdfe227
superColin <supercolin.dev@gmail.com>
2020-03-19 11:15:46 -0400

    day 94

---
+++ 094__pow.py
@@ -0,0 +1,16 @@
+# 3/19/2020
+
+# https://docs.python.org/3/library/functions.html
+ 
+
+print(pow(2,9))
+print(pow(2,10))
+print(pow(2,11))
+print(pow(2,12))
+print(pow(2,13))
+print(pow(2,14))
+print(pow(2,15))
+print(pow(2,16))
+
+
+
