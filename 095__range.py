d0fe8bfde1658a7a27dbbc0bf59c073e25d38530
superColin <supercolin.dev@gmail.com>
2020-03-20 08:30:48 -0400

    day 95! Almost there!

---
+++ 095__range.py
@@ -0,0 +1,18 @@
+# 3/20/2020
+
+# https://docs.python.org/3/library/functions.html
+# https://www.w3schools.com/python/ref_func_range.asp
+
+
+rangeX = range(10)
+rangeY = range(10,20)
+rangeZ = range(0, 20, 2)
+
+
+# print(list(rangeX))
+print(rangeX)
+print(rangeY)
+print(rangeZ)
+
+
+# Includes the starting point but not the end point
