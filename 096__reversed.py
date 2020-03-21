ec0bf4f06791765564c760351b6e024f85302e9d
superColin <supercolin.dev@gmail.com>
2020-03-21 11:47:32 -0400

    day 96!

---
+++ 096__reversed.py
@@ -0,0 +1,13 @@
+# 3/21/2020
+
+# https://docs.python.org/3/library/functions.html
+
+
+numberArray = [0,1,2,3,4,5,6,7,8,9]
+
+print(numberArray)
+# print(reversed(numberArray)) # Wont work
+print(list(reversed(numberArray)))
+
+
+
