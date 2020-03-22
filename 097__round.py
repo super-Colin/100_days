eb9a2b7e5493e14c557878e1ea9a962ee4f46986
superColin <supercolin.dev@gmail.com>
2020-03-22 03:28:36 -0400

    day 97!

---
+++ 097__round.py
@@ -0,0 +1,11 @@
+# 3/22/2020
+
+# https://docs.python.org/3/library/functions.html
+
+numberToRound = 17.53154762478
+
+print(round(numberToRound))
+print(round(numberToRound, 3))
+print(round(numberToRound, 6))
+
+
