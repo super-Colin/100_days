5a8af1e82290d4039ad22302a93971df09ec739d
superColin <supercolin.dev@gmail.com>
2020-03-23 09:09:43 -0400

    day 98!!

---
+++ 098__slice.py
@@ -0,0 +1,13 @@
+# 3/23/2020
+
+# https://docs.python.org/3/library/functions.html
+
+
+
+newList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+
+print(newList[slice(15)])
+print(newList[slice(3, 15)])
+print(newList[slice(3, 15, 2)])
+
+
